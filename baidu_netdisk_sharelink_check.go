d75b74ae34bfd89fa2fa4998b0060fe73bab079c
xm <xiaxianmao@thefifthera.com>
2019-08-21 11:16:11 +0800

    变量名调整

--- baidu_netdisk_sharelink_check.go
+++ baidu_netdisk_sharelink_check.go
@@ -6,7 +6,7 @@ import (
 	"github.com/irebit/baidu_netdisk_sharelink_check/request"
 )
 
-const DistincPrefixURL = "https://pan.baidu.com/share/init?surl="
+const DestURLPrefix = "https://pan.baidu.com/share/init?surl="
 
 type ShareLink string
 
@@ -17,7 +17,7 @@ type ShareLink string
 func (l ShareLink) Check() (bool, error) {
 	_, resp, err := request.Get(string(l))
 	if len(resp.Header["Location"]) == 1 {
-		if strings.Contains(resp.Header["Location"][0], DistincPrefixURL) {
+		if strings.Contains(resp.Header["Location"][0], DestURLPrefix) {
 			return true, err
 		}
 	}
