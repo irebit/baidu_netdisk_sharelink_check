5912ab58c744ae01d3d7d6e70e87af00fc287b4f
xm <xiaxianmao@thefifthera.com>
2019-08-21 11:06:37 +0800

    优化调用方式

--- baidu_netdisk_sharelink_check.go
+++ baidu_netdisk_sharelink_check.go
@@ -1,7 +1,6 @@
 package baidu_netdisk_sharelink_check
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/irebit/baidu_netdisk_sharelink_check/request"
@@ -11,11 +10,12 @@ const DistincPrefixURL = "https://pan.baidu.com/share/init?surl="
 
 type ShareLink string
 
-func (l ShareLink) ToString() string {
-	return fmt.Sprintf("%v", l)
-}
-func (l *ShareLink) Check() (bool, error) {
-	_, resp, err := request.Get(l.ToString())
+// func (l ShareLink) ToString() string {
+// 	return fmt.Sprintf("%v", l)
+// }
+
+func (l ShareLink) Check() (bool, error) {
+	_, resp, err := request.Get(string(l))
 	if len(resp.Header["Location"]) == 1 {
 		if strings.Contains(resp.Header["Location"][0], DistincPrefixURL) {
 			return true, err
--- baidu_netdisk_sharelink_check_test.go
+++ baidu_netdisk_sharelink_check_test.go
@@ -7,9 +7,7 @@ import (
 
 func TestCheck(t *testing.T) {
 	//suc
-	var link ShareLink = "https://pan.baidu.com/s/1D_vNhmirNjGC3gkdxwGPYw"
-	log.Println(link.Check())
+	log.Println(ShareLink("https://pan.baidu.com/s/1D_vNhmirNjGC3gkdxwGPYw").Check())
 	//fail
-	var link2 ShareLink = "https://pan.baidu.com/s/1bQRjzr8x7XvKj8L3_iv9xw"
-	log.Println(link2.Check())
+	log.Println(ShareLink("https://pan.baidu.com/s/1bQRjzr8x7XvKj8L3_iv9xw").Check())
 }
